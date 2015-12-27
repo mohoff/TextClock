6a596ba3fa6085b18e047604067e7e5e670b1d12
Moritz Hoffmann <mohoff@web.de>
2015-12-27 21:15:02 +0100

    Changed vncserver start from background to normal

--- startup.sh
+++ startup.sh
@@ -67,9 +67,6 @@ else
   set_time_manually
 fi
 
-# OPTIONAL: Start vnc server to enable remote connections with remote GUI (in Ubuntu clients you can use preinstalled tool Remmina in order to connect with vnc server)
-vncserver :1 -geometry 1280x1024 -depth 16 -pixelformat rgb565 &
-
 # Removes mouse cursor from display when idle
 unclutter &
 
@@ -78,3 +75,8 @@ pkill -f chromium
 
 # You can cancel kiosk mode with ALT+F4 or by killing the process in any way
 chromium /home/pi/lcduhr/index.html --kiosk --incognito
+
+# OPTIONAL: Start vnc server to enable remote connections with remote GUI (in Ubuntu clients you can use preinstalled tool Remmina in order to connect with vnc server)
+vncserver :1 -geometry 1280x1024 -depth 16 -pixelformat rgb565
+
+
