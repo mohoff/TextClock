5e9d57e57e9ac3c7ee7fce629fe1f0db4fec0137
Moritz Hoffmann <mohoff@web.de>
2015-12-27 21:21:11 +0100

    disabled vncserver start. commented out

--- startup.sh
+++ startup.sh
@@ -77,6 +77,6 @@ pkill -f chromium
 chromium /home/pi/lcduhr/index.html --kiosk --incognito
 
 # OPTIONAL: Start vnc server to enable remote connections with remote GUI (in Ubuntu clients you can use preinstalled tool Remmina in order to connect with vnc server)
-vncserver :1 -geometry 1280x1024 -depth 16 -pixelformat rgb565
+#vncserver :1 -geometry 1280x1024 -depth 16 -pixelformat rgb565
 
 
