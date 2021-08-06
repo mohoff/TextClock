4acb06b5c0add88265d0a24c711e1c9ada03b0e5
Till Hoffmann <till.hoffmann@enforge.de>
2021-08-06 10:15:25 +0200

    simplified path & minor fixes

--- screenandmail.sh
+++ screenandmail.sh
@@ -3,6 +3,6 @@
 timestamp=$(date +%Y%b%d_%H-%M-%S)
 # e.g. 2016Jan25_00-40-06
 
-scrot ~/lcduhr_screenshots/$timestamp.png
+scrot ~/TextClock_screenshots/$timestamp.png
 
-echo "Siehe Anlage..." | mutt -a "~/lcduhr_screenshots/$timestamp.png" -s "Neuer Screenshot von Raspberry Pi" -- hoffmamo@gmail.com
+echo "Siehe Anlage..." | mutt -a "~/TextClock_screenshots/$timestamp.png" -s "Neuer Screenshot von Raspberry Pi" -- hoffmamo@gmail.com
--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#lxterminal -e bash /home/pi/lcduhr/time.sh
 
 # Removes mouse cursor from display when idle
 unclutter &
@@ -8,7 +7,4 @@ unclutter &
 pkill -f chromium
 
 # You can cancel kiosk mode with ALT+F4 or by killing the process in any way
-chromium /home/pi/lcduhr/index.html --kiosk --incognito
-
-# OPTIONAL: Start vnc server to enable remote connections with remote GUI (in Ubuntu clients you can use preinstalled tool Remmina in order to connect with vnc server)
-#vncserver :1 -geometry 1280x1024 -depth 16 -pixelformat rgb565
+chromium-browser /home/pi/TextClock/index.html --kiosk --incognito
